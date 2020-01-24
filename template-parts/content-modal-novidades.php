ab273d205988f40bb86e5cad085d41b7d12dc02d
Sarah86 <sarah.goncalves@gmail.com>
2020-01-24 14:09:30 +0100

    ajax fitler for posts

--- template-parts/content-modal-novidades.php
+++ template-parts/content-modal-novidades.php
@@ -23,11 +23,33 @@
 </div>
 
 <div class="categories">
-<ul>
-    <?php wp_list_categories( array(
-        'orderby'    => 'name',
-        'show_count' => true,
-        'include' => array( 19, 26, 28 )
-    ) ); ?> 
-</ul>
+    <ul>
+        <?php wp_list_categories(array(
+            'orderby'    => 'name',
+            'show_count' => true,
+            'include' => array(19, 26, 28)
+        )); ?>
+    </ul>
+</div>
+
+<div>
+<form action="<?php echo site_url() ?>/wp-admin/admin-ajax.php" method="POST" id="filter">
+    <?php
+        if ($terms = get_terms(array(
+                    'taxonomy' => 'category', 
+                    'orderby' => 'name',
+                    'include' => array(19, 26, 28)
+                    ))
+            ) :
+
+            echo '<ul class="categoryfilter">';
+            foreach ($terms as $term) :
+                echo '<li id="' . $term->term_id . '">' . $term->name . '</li>'; // ID of the category as the value of an option
+            endforeach;
+            echo '</ul>';
+        endif;
+    ?>
+    <input type="hidden" name="action" value="myfilter">
+</form>
+<div id="response"></div>
 </div>
